bcdaf34f702784188befce4845a1978e9f72ae06
Paleolítik <edicionespaleoliticas@gmail.com>
2024-07-21 03:39:16 -0300

    Update scripts.mjs

--- public/scripts.mjs
+++ public/scripts.mjs
@@ -211,7 +211,7 @@ async function filtrarBase() {
 
     try {
     const parametros = seleccion.map(opcion => `icono=${opcion}`).join('&');
-    const url = `http://localhost:1234/sitios?${parametros}`;
+    const url = `https://arqueoapp.onrender.com//sitios?${parametros}`;
 
     const response = await fetch(url);
     const json = await response.json();
