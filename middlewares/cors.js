1bdb41e3edf0982d714e6b97c4f2a659950d1dbe
Paleolítik <edicionespaleoliticas@gmail.com>
2024-07-21 02:34:24 -0300

    Update cors.js

--- middlewares/cors.js
+++ middlewares/cors.js
@@ -4,7 +4,7 @@ const ACCEPTED_ORIGINS = [
   'http://localhost:5500',
   'http://localhost:1234',
   'http://localhost:8080',
-  //'http://localhost:3306'
+  `http://localhost:${process.env.PORT}`
 ]
 
 export const corsMiddleware = ({ acceptedOrigins = ACCEPTED_ORIGINS } = {}) => cors({
