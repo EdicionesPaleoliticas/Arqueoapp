fac30723aa7d63ab48313b5e7839521718af8c45
Paleolítik <edicionespaleoliticas@gmail.com>
2024-07-22 02:36:55 -0300

    Update scripts.mjs

--- public/scripts.mjs
+++ public/scripts.mjs
@@ -205,16 +205,17 @@ async function filtrarBase() {
     const response = await fetch(url);
     const json = await response.json();
     console.log(json);
+    for (let i = 0; i < json.sitios.length; i++) {
+    if (json.sitios[i].icono === `https://arqueoapp.onrender.com/img/icono_${selectElement.value}.png`){               
+    guardarElemento(sitios[i]);
+        }
+    }
     
     } catch (err) {
     console.error(err);
     }
     
-    for (let i = 0; i < json.sitios.length; i++) {
-        if (json.sitios[i].icono === `https://arqueoapp.onrender.com/img/icono_${selectElement.value}.png`){               
-        guardarElemento(sitios[i]);
-        }
-    }
+
 }
 
 const borrarTodoBtn = document.getElementById('btn-limpiar-todo');
