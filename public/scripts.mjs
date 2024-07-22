1b6751da50a3be17c8074d52e4fc1812f3794a12
Paleolítik <edicionespaleoliticas@gmail.com>
2024-07-22 03:00:42 -0300

    Update scripts.mjs

--- public/scripts.mjs
+++ public/scripts.mjs
@@ -214,7 +214,7 @@ async function filtrarBase() {
     for (let i = 0; i < json.sitios.length; i++) {
     if (json.sitios[i].icono === `https://arqueoapp.onrender.com/img/icono_${selectedValue}.png`){               
     guardarElemento(json.sitios[i]);
-        console.log(json.sitios[i];
+        console.log(json.sitios[i]);
         }
     }
     
